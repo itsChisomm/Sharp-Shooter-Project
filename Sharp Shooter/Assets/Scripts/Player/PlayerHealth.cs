4a51ed796d11ed4174b9f8bcc07567d2246ddb45
itsChisomm <chisomc@hotmail.co.uk>
2025-09-06 14:07:57 +0100

    Shield UI & Enemy Spawns

--- Sharp Shooter/Assets/Scripts/Player/PlayerHealth.cs
+++ Sharp Shooter/Assets/Scripts/Player/PlayerHealth.cs
@@ -1,11 +1,14 @@
 using Cinemachine;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
+    [Range(1,10)]
     [SerializeField] int startingHealth = 5;
     [SerializeField] CinemachineVirtualCamera deatVirtualCamera;
     [SerializeField] Transform weaponCamera;
+    [SerializeField] Image[] shieldBars;
 
     int currentHealth;
     int gameOverVirtualCameraPriorty = 20;
@@ -13,19 +16,36 @@ public class PlayerHealth : MonoBehaviour
     void Awake()
     {
         currentHealth = startingHealth;
+        AdjustShieldUI();
     }
 
 
     public void TakeDamage(int amount) // amount of damage to take
     {
         currentHealth -= amount;
-        Debug.Log(amount + " damage taken");
+        AdjustShieldUI();
+
 
         if (currentHealth <= 0)
         {
             weaponCamera.parent = null;
             deatVirtualCamera.Priority = gameOverVirtualCameraPriorty;
             Destroy(this.gameObject);
+        }
+    }
+
+    void AdjustShieldUI()
+    {
+        for (int i = 0; i < shieldBars.Length; i++)
+        {
+            if (i < currentHealth)
+            {
+                shieldBars[i].gameObject.SetActive(true);
+            }
+            else
+            {
+                shieldBars[i].gameObject.SetActive(false);
+            }
         }
     }
 }
